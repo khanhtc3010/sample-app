07b663b1cbf42b5b8e97957cc81336ae92399fa1
khanhtc3010 <khanhtran0230@gmail.com>
2016-07-15 10:15:30 +0700

    follow user with ajax

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,14 @@ SampleApp::Application.routes.draw do
   get  '/login',  to: 'sessions#new'
   post  '/login',  to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :entries,  only: [:create, :destroy]
   resources :comments,  only: [:create, :destroy]
+  resources :relationships,  only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
